e119f59b284de2035dea593cadc773613f0b4e62
Luis Escobar <lescobar.099@gmail.com>
2020-10-04 16:48:22 -0600

    body parser + restructuring

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require("express");
 // Importar handelbars como template engine
 const exphbs = require("express-handlebars");
 
+//Importar Body parser que nos permite accerder al cuerpo de la peticion HTTP
+const bodyParser = require("body-parser");
+
+//Importar la funcion de calculo de metodo frances
+const { calcularMetodoFrances} = require("./calculoMetodoFrances");
+
 // Crear un servidor express
 const app = express();
 
@@ -12,9 +18,22 @@ app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs"}));
 
 app.set("view engine" , "hbs");
 
+// Habilitar body parser para eer los datos del cuerpo de peticiones POST
+app.use(bodyParser.urlencoded({ extended: true}));
+
 // Crear una ruta para /
 app.get("/", (req, res, next) => {
     res.render("formulario_prestamo");
+});
+
+app.post("/prestamo", (req, res, next) => {
+    // Asignacion por destructuring
+    const { monto, tasaInteres, periodo} = req.body;
+    //console.log(req.body);
+
+    calcularMetodoFrances(monto, tasaInteres, periodo);
+
+    res.send("Peticion aceptada");
 });
 
 // Inicializar el servidor en un puerto en especifico
