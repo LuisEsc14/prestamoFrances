9e0fe2cb50fba7444c6fcb2356d32e728a31609b
Luis Escobar <lescobar.099@gmail.com>
2020-10-04 12:23:49 -0600

    Creacion del servidor ejecutandose en el puerto 5000 con un endpoint en /

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ app.get("/", (req, res, next) => {
     res.send("Bienvenido");
 });
 
-app.get("/carrito", (req, res, next) =>{
-    res.send("Este es un carrito de compras");
-});
-
 // Inicializar el servidor en un puerto en especifico
 app.listen(5000, () => {
     console.log("Servidor ejecutandose en el puerto 5000");
